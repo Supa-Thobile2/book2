a5a20328ef9617ed23d792eee64ede3c96e9095f
Supa-Thobile2 <thobilenontenja@gmail.com>
2022-08-11 12:10:34 +0200

    updated project

---
+++ mainjs/index.js
@@ -0,0 +1,26 @@
+
+
+// let lastName = document.getElementById("lname").value;
+// let phoneNumber = document.getElementById("pnumber").value;
+// let email = document.getElementById("email").value;
+// let image = document.getElementById("myFile").value;
+let user = []
+console.log(user)
+
+function registerUser(){
+    let firstName = document.getElementById("fname").value;
+    let lastName = document.getElementById("lname").value;
+    let phoneNumber = document.getElementById("pnumber").value;
+    let email = document.getElementById("email").value;
+    let image = document.getElementById("myFile").value;
+    console.log(firstName, lastName, phoneNumber, email, image)
+    
+    
+    
+}
+registerUser()
+
+
+
+
+
---
+++ mainjs/login.js
@@ -0,0 +1,14 @@
+function login(){
+    
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("pwd").value;
+    console.log(email, password)
+
+    // for(i=0; i<user.length; i++){
+
+    // }
+    
+    
+    
+}
+login()
